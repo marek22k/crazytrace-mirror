3face55a70e93df62a3957a74ac99faefc6b315d
Marek Küthe <m.k@mk16.de>
2025-07-04 19:03:08 +0000

    Use reunse for debian package

--- debian/upstream/metadata.ex
+++ debian/upstream/metadata.ex
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024 Marek Küthe <m.k@mk16.de>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 # See https://wiki.debian.org/UpstreamMetadata for more info/fields.
 
 Bug-Database: https://codeberg.org/mark22k/crazytrace/issues
