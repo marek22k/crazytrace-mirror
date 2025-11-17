51df259ac4ecb604651eddc86a1a28388a79b157
Marek Küthe <m.k@mk16.de>
2025-11-17 14:34:33 +0000

    Add log

--- src/capability_managment.cpp
+++ src/capability_managment.cpp
@@ -6,8 +6,12 @@ void CapabilityManagment::check_for_capabilites() {
     if (! capng_have_capability(CAPNG_PERMITTED, CAP_NET_ADMIN))
         throw std::runtime_error("Missing CAP_NET_ADMIN capability.");
 
+    BOOST_LOG_TRIVIAL(debug) << "CAP_NET_ADMIN capability available.";
+
     if (! capng_have_capability(CAPNG_PERMITTED, CAP_SETPCAP))
         throw std::runtime_error("Missing CAP_SETPCAP capability.");
+
+    BOOST_LOG_TRIVIAL(debug) << "CAP_SETPCAP capability available.";
 }
 
 void CapabilityManagment::drop_capabilies() {
@@ -26,12 +30,16 @@ void CapabilityManagment::drop_capabilies() {
         throw std::runtime_error("Failed to set bounding set CAP_SETPCAP capability.");
     if (capng_apply(CAPNG_SELECT_ALL) != 0)
         throw std::runtime_error("Failed to apply capabilities.");
+
+    BOOST_LOG_TRIVIAL(debug) << "All capabilities except CAP_NET_ADMIN and CAP_SETPCAP dropped.";
 }
 
 void CapabilityManagment::drop_all_capabilies() {
     capng_clear(CAPNG_SELECT_ALL);
     if (capng_apply(CAPNG_SELECT_ALL) != 0)
         throw std::runtime_error("Failed to apply capabilities.");
+
+    BOOST_LOG_TRIVIAL(debug) << "All capabilities dropped.";
 }
 
 #endif
