80348000b6147582cfba037f27186142f0be6eff
Marek Küthe <m.k@mk16.de>
2025-11-24 15:39:49 +0000

    Format

--- src/ip_ranges.hpp
+++ src/ip_ranges.hpp
@@ -9,7 +9,9 @@
 
 namespace ip_ranges
 {
-    const Tins::AddressRange<Tins::IPv6Address> linklocal_range = // NOLINT(bugprone-throwing-static-initialization, cert-err58-cpp)
+    const Tins::AddressRange<Tins::IPv6Address>
+        linklocal_range = // NOLINT(bugprone-throwing-static-initialization,
+                          // cert-err58-cpp)
         Tins::IPv6Address("fe80::") / 10;
 }
 
