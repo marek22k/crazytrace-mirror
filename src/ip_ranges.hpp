fc82d272aee2d393d9fb84f9b567a11cf7ef8c24
Marek Küthe <m.k@mk16.de>
2025-11-24 01:12:42 +0000

    Add license

--- src/ip_ranges.hpp
+++ src/ip_ranges.hpp
@@ -1,5 +1,9 @@
-#ifndef TINS_EXTENSIONS
-#define TINS_EXTENSIONS
+// SPDX-FileCopyrightText: Copyright (C) 2024-2025 Marek Küthe <m.k@mk16.de>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef IP_RANGES_EXTENSIONS
+#define IP_RANGES_EXTENSIONS
 
 #include <tins/tins.h>
 
