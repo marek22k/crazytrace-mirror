a3bc75b0501767725bd107eddb79d649033a6532
Marek Küthe <m.k@mk16.de>
2025-12-09 22:23:09 +0000

    Improve code style

--- src/capability_managment.cpp
+++ src/capability_managment.cpp
@@ -8,77 +8,77 @@
 
 void CapabilityManagment::lock()
 {
-    if (capng_lock() != 0)
+    if (::capng_lock() != 0)
         throw std::system_error(
             errno, std::generic_category(), "Failed to lock capabilities");
 }
 
 void CapabilityManagment::check_for_capabilites()
 {
-    if (capng_have_capability(CAPNG_PERMITTED, CAP_NET_ADMIN) != 1)
+    if (::capng_have_capability(CAPNG_PERMITTED, CAP_NET_ADMIN) != 1)
         throw std::runtime_error("Missing CAP_NET_ADMIN capability");
 
-    if (capng_have_capability(CAPNG_PERMITTED, CAP_SETPCAP) != 1)
+    if (::capng_have_capability(CAPNG_PERMITTED, CAP_SETPCAP) != 1)
         throw std::runtime_error("Missing CAP_SETPCAP capability");
 }
 
 void CapabilityManagment::drop_capabilies()
 {
-    capng_clear(CAPNG_SELECT_ALL);
+    ::capng_clear(CAPNG_SELECT_ALL);
 
-    if (capng_update(CAPNG_ADD, CAPNG_EFFECTIVE, CAP_NET_ADMIN) != 0)
+    if (::capng_update(CAPNG_ADD, CAPNG_EFFECTIVE, CAP_NET_ADMIN) != 0)
         throw std::system_error(
             errno,
             std::generic_category(),
             "Failed to set effective CAP_NET_ADMIN capability");
-    if (capng_update(CAPNG_ADD, CAPNG_PERMITTED, CAP_NET_ADMIN) != 0)
+    if (::capng_update(CAPNG_ADD, CAPNG_PERMITTED, CAP_NET_ADMIN) != 0)
         throw std::system_error(
             errno,
             std::generic_category(),
             "Failed to set permitted CAP_NET_ADMIN capability");
-    if (capng_update(CAPNG_ADD, CAPNG_BOUNDING_SET, CAP_NET_ADMIN) != 0)
+    if (::capng_update(CAPNG_ADD, CAPNG_BOUNDING_SET, CAP_NET_ADMIN) != 0)
         throw std::system_error(
             errno,
             std::generic_category(),
             "Failed to set bounding set CAP_NET_ADMIN capability");
 
     // Allow post up commands to have CAP_NET_ADMIN permission
-    if (capng_update(CAPNG_ADD, CAPNG_INHERITABLE, CAP_NET_ADMIN) != 0)
+    if (::capng_update(CAPNG_ADD, CAPNG_INHERITABLE, CAP_NET_ADMIN) != 0)
         throw std::system_error(
             errno,
             std::generic_category(),
             "Failed to set ambient CAP_NET_ADMIN capability");
-    if (capng_update(CAPNG_ADD, CAPNG_AMBIENT, CAP_NET_ADMIN) != 0)
+    if (::capng_update(CAPNG_ADD, CAPNG_AMBIENT, CAP_NET_ADMIN) != 0)
         throw std::system_error(
             errno,
             std::generic_category(),
             "Failed to set ambient CAP_NET_ADMIN capability");
 
-    if (capng_update(CAPNG_ADD, CAPNG_EFFECTIVE, CAP_SETPCAP) != 0)
+    if (::capng_update(CAPNG_ADD, CAPNG_EFFECTIVE, CAP_SETPCAP) != 0)
         throw std::system_error(
             errno,
             std::generic_category(),
             "Failed to set effective CAP_SETPCAP capability");
-    if (capng_update(CAPNG_ADD, CAPNG_PERMITTED, CAP_SETPCAP) != 0)
+    if (::capng_update(CAPNG_ADD, CAPNG_PERMITTED, CAP_SETPCAP) != 0)
         throw std::system_error(
             errno,
             std::generic_category(),
             "Failed to set permitted CAP_SETPCAP capability");
-    if (capng_update(CAPNG_ADD, CAPNG_BOUNDING_SET, CAP_SETPCAP) != 0)
+    if (::capng_update(CAPNG_ADD, CAPNG_BOUNDING_SET, CAP_SETPCAP) != 0)
         throw std::system_error(
             errno,
             std::generic_category(),
             "Failed to set bounding set CAP_SETPCAP capability");
-    if (capng_apply(CAPNG_SELECT_ALL) != 0)
+    if (::capng_apply(CAPNG_SELECT_ALL) != 0)
         throw std::system_error(
             errno, std::generic_category(), "Failed to apply capabilities");
 }
 
 void CapabilityManagment::drop_all_capabilies()
 {
-    capng_clear(CAPNG_SELECT_ALL);
+    ::capng_clear(CAPNG_SELECT_ALL);
 
-    if (capng_apply(CAPNG_SELECT_ALL) != 0)
+    if (::capng_apply(CAPNG_SELECT_ALL) != 0)
         throw std::system_error(
             errno, std::generic_category(), "Failed to apply capabilities");
 }
--- src/seccomp.cpp
+++ src/seccomp.cpp
@@ -7,7 +7,7 @@
 #ifdef HAVE_SECCOMP
 
 SeccompFilterContext::SeccompFilterContext(uint32_t def_action) :
-    ctx(seccomp_init(def_action))
+    ctx(::seccomp_init(def_action))
 {
     if (this->ctx == nullptr)
         throw std::system_error(
@@ -19,7 +19,7 @@ void SeccompFilterContext::rule_add(uint32_t action, int syscall) const
     if (!this->is_useable())
         throw std::runtime_error("seccomp filter context not useable.");
 
-    seccomp_rule_add(this->ctx, action, syscall, 0);
+    ::seccomp_rule_add(this->ctx, action, syscall, 0);
 }
 
 void SeccompFilterContext::allow(int syscall) const
@@ -898,7 +898,7 @@ void SeccompFilterContext::load() const
     if (!this->is_useable())
         throw std::runtime_error("seccomp filter context not useable.");
 
-    if (seccomp_load(this->ctx) != 0)
+    if (::seccomp_load(this->ctx) != 0)
         throw std::system_error(
             errno, std::generic_category(), "Failed to load seccomp filter.");
 }
@@ -908,7 +908,7 @@ void SeccompFilterContext::reset(uint32_t def_action) const
     if (!this->is_useable())
         throw std::runtime_error("seccomp filter context not useable.");
 
-    if (seccomp_reset(this->ctx, def_action) != 0)
+    if (::seccomp_reset(this->ctx, def_action) != 0)
         throw std::system_error(
             errno, std::generic_category(), "Failed to reset seccomp filter.");
 }
@@ -918,7 +918,7 @@ void SeccompFilterContext::release()
     if (!this->is_useable())
         throw std::runtime_error("seccomp filter context not useable.");
 
-    seccomp_release(this->ctx);
+    ::seccomp_release(this->ctx);
     this->ctx = nullptr;
 }
 
