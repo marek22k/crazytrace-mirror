6c0cfc481ff87e7ae07bf15224f614e17789db94
Marek Küthe <m.k@mk16.de>
2026-04-12 23:44:39 +0000

    Add capsicum support

---
+++ src/capsicum.hpp
@@ -0,0 +1,62 @@
+// SPDX-FileCopyrightText: Copyright (C) 2026 Marek Küthe <m.k@mk16.de>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef CAPSICUM_HPP
+#define CAPSICUM_HPP
+
+#ifdef HAVE_CAPSICUM
+
+    #include <system_error>
+    #include <cerrno>
+    #include <capsicum_helpers.h>
+    #include <sys/capsicum.h>
+
+namespace Capsicum
+{
+
+    inline void enter()
+    {
+        if (::caph_enter() != 0)
+            throw std::system_error(
+                errno,
+                std::generic_category(),
+                "Failed to enter capability mode");
+    }
+
+    inline void limit_stdio()
+    {
+        if (::caph_limit_stdio() != 0)
+            throw std::system_error(
+                errno,
+                std::generic_category(),
+                "Failed to limit stdio");
+    }
+
+    inline bool in_capability_mode()
+    {
+        unsigned int flag;
+        if (::cap_getmode(&flag) != 0)
+            throw std::system_error(
+                errno,
+                std::generic_category(),
+                "Failed to get capability mode");
+        
+        return flag != 0;
+    }
+
+    template<typename... Rights>
+    inline void limit_rights(int fd, Rights... rights) {
+        cap_rights_t setrights;
+        cap_rights_init(&setrights, rights..., 0);
+        if (::caph_rights_limit(fd, &setrights) != 0)
+            throw std::system_error(
+                errno,
+                std::generic_category(),
+                "Failed to limit rights");
+    }
+}
+
+#endif
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (C) 2024-2025 Marek Küthe <m.k@mk16.de>
+// SPDX-FileCopyrightText: Copyright (C) 2024-2026 Marek Küthe <m.k@mk16.de>
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -20,6 +20,7 @@
 #include "nodecontainer.hpp"
 #include "seccomp.hpp"
 #include "tun_tap.hpp"
+#include "capsicum.hpp"
 
 int main(int argc, char * argv[])
 {
@@ -128,6 +129,12 @@ int main(int argc, char * argv[])
         BOOST_LOG_TRIVIAL(info) << "Landlock: false";
 #endif
 
+#ifdef HAVE_CAPSICUM
+        BOOST_LOG_TRIVIAL(info) << "capsicum: true";
+#else
+        BOOST_LOG_TRIVIAL(info) << "capsicum: false";
+#endif
+
         const std::shared_ptr<crazytrace::NodeContainer> nodecontainer =
             config.get_node_container();
 
@@ -185,6 +192,20 @@ int main(int argc, char * argv[])
         seccomp_context.kill_signal();
         seccomp_context.load();
         seccomp_context.release();
+#endif
+#ifdef HAVE_CAPSICUM
+        Capsicum::enter();
+        Capsicum::limit_stdio();
+        Capsicum::limit_rights(dev.native_handler(), CAP_EVENT, CAP_FCNTL, CAP_IOCTL, CAP_READ, CAP_WRITE);
+        
+        if (Capsicum::in_capabiliy_mode())
+        {
+            BOOST_LOG_TRIVIAL(info) << "capsicum capabiliy mode: true";
+        }
+        else
+        {
+            BOOST_LOG_TRIVIAL(info) << "capsicum capabiliy mode: false";
+        }
 #endif
 
         const crazytrace::Crazytrace ct(
